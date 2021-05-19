6e6def556f08a0f19ad41a4628806d5a93f968a1
HungNT <hungntse01@gmail.com>
2021-05-19 11:10:02 +0700

    initial master

---
+++ src/main/java/com/gcl/crm/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.gcl.crm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String homepage() {
+        return "index";
+    }
+}
