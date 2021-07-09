c00a01d8e0447740758ba42be5eae2c4c91cfcd7
HungNT <hungntse01@gmail.com>
2021-07-09 19:35:31 +0700

    UI: Take Care Potential V.2

--- src/main/resources/static/js/datatables/datatables-custom.js
+++ src/main/resources/static/js/datatables/datatables-custom.js
@@ -1,66 +1,45 @@
 let urlNewStatistic = 'http://localhost:8081/statistic/potential/today';
 
-let dataTableStatisticPotential = new simpleDatatables.DataTable("#datatablesStatisticPotential", {
-    fixedHeight: true,
-    searchable: false,
-    perPageSelect: false,
-    perPage: 3,
-    ajax: {
-        url: urlNewStatistic
-    }
-});
-
-fetch(urlNewStatistic).then(
-    res=>{
-        res.json().then(
-            data=>{
-                console.log(data);
-                if (data.length > 0) {
-                    var slag = data.map(doc => Object.values(doc));
-                    dataTableStatisticPotential.rows().add(slag);
-                }
-            }
-        )
-    }
-)
-
-$(function() {
-    $('#reloadNewPotentials').on('click', function () {
-        $.ajax({
-            url: urlNewStatistic,
-            dataType: "text/json",
-            success: function(result){
-                console.log(result);
-            }
-        })
+if (document.getElementById("datatablesStatisticPotential") != null) {
+    let dataTableStatisticPotential = new simpleDatatables.DataTable("#datatablesStatisticPotential", {
+        fixedHeight: true,
+        searchable: false,
+        perPageSelect: false,
+        perPage: 3,
     });
-})
-
-const dataTableTakeCare = new simpleDatatables.DataTable(
-    "#datatablesTakeCareOfPotential",
-    {
-        fixedHeight: true,
-        searchable: false,
-        perPageSelect: false,
-        perPage: 3,
-    }
-);
-
-const datatablesPreQualifiedOfPotential = new simpleDatatables.DataTable(
-    "#datatablesPreQualifiedOfPotential",
-    {
-        fixedHeight: true,
-        searchable: false,
-        perPageSelect: false,
-        perPage: 3,
-    }
-);
-const datatablesQualifiedOfPotential = new simpleDatatables.DataTable(
-    "#datatablesQualifiedOfPotential",
-    {
-        fixedHeight: true,
-        searchable: false,
-        perPageSelect: false,
-        perPage: 3,
-    }
-);
+    fetch(urlNewStatistic).then(
+        res=>{
+            res.json().then(
+                data=>{
+                    console.log(data);
+                    if (data.length > 0) {
+                        var slag = data.map(doc => Object.values(doc));
+                        dataTableStatisticPotential.rows().add(slag);
+                    }
+                }
+            )
+        }
+    )
+} else if (document.getElementById("datatablesTakeCareOfPotential") != null) {
+    const dataTableTakeCare = new simpleDatatables.DataTable(
+        "#datatablesTakeCareOfPotential",
+        {
+            fixedHeight: true,
+            searchable: false,
+            perPageSelect: false,
+            perPage: 3,
+        }
+    );
+} else if (document.getElementById("datatablesManageTakeCareOfPotential") != null) {
+    const datatablesTakeCareOfPotential = new simpleDatatables.DataTable(
+        "#datatablesManageTakeCareOfPotential",
+        {
+            searchable: false,
+            fixedHeight: true,
+            perPageSelect: false,
+            labels: {
+                info: "",
+            }
+        }
+    );
+}
